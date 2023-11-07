54b63121d93a67a1b2491642110f83dda1c39bdb
terenjit <terenjit@LAPTOP-TCN8983R>
2023-11-07 13:33:48 +0700

    unit test for CRUD

---
+++ db/sqlc/main_test.go
@@ -0,0 +1,28 @@
+package sqlc
+
+import (
+	"context"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+const (
+	dbDriver = "postgres"
+	dbSource = "postgresql://postgres:test123@localhost:5433/simple_bank?sslmode=disable"
+)
+
+var testQueries *Queries
+
+func TestMain(m *testing.M) {
+	conn, err := pgxpool.New(context.Background(), dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db", err)
+	}
+
+	testQueries = New(conn)
+
+	os.Exit(m.Run())
+}
